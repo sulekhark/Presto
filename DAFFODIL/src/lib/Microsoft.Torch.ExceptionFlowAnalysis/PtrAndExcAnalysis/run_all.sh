c62965ba4f77089c88dfe6c7f50916b4d5c922e5
Sulekha Kulkarni <sulekha.kulkarni@gmail.com>
2019-09-17 08:15:48 -0400

    Fixes to the datalog analyses and python/shell scripts to run z3 in Linux

---
+++ DAFFODIL/src/lib/Microsoft.Torch.ExceptionFlowAnalysis/PtrAndExcAnalysis/run_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -f full_run.out
+
+z3 CIPtrAnalysis.datalog > cipa_analysis.out 2>&1
+python3 parse_z3_out.py CIPtrAnalysis.datalog cipa_analysis.out >> full_run.out 2>&1
+
+z3 ExcAnalysisIntraProc.datalog > exc_intra.out 2>&1
+python3 parse_z3_out.py ExcAnalysisIntraProc.datalog exc_intra.out >> full_run.out 2>&1
+
+z3 ExcAnalysisInterProc.datalog > exc_inter.out 2>&1
+python3 parse_z3_out.py ExcAnalysisInterProc.datalog exc_inter.out >> full_run.out 2>&1
+
+z3 ExcFlows.datalog > exc_flows.out 2>&1
+python3 parse_z3_out.py ExcFlows.datalog exc_flows.out >> full_run.out 2>&1
