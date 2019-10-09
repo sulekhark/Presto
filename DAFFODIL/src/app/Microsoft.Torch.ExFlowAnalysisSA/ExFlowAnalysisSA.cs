c9675ca99e2f55657aa2367d7ca4d05d2de1f2ea
sulekhark <sulekha.kulkarni@gmail.com>
2019-10-09 15:51:13 -0400

    clean up

--- DAFFODIL/src/app/Microsoft.Torch.ExFlowAnalysisSA/ExFlowAnalysisSA.cs
+++ DAFFODIL/src/app/Microsoft.Torch.ExFlowAnalysisSA/ExFlowAnalysisSA.cs
@@ -9,32 +9,13 @@ namespace Microsoft.Torch.ExFlowAnalysisSA
     {
         static void Main()
         {
-            ConfigParams.Z3ExePath = @"C:\Users\torch\work\DAFFODIL\DAFFODIL\packages\Z3-4.1\bin\z3.exe";
-            ConfigParams.StubsPath = @"C:\Users\torch\work\DAFFODIL\DAFFODIL\src\stubs\Microsoft.Torch.Stubs\bin\Debug\Microsoft.Torch.Stubs.dll";
-            ConfigParams.AnalysesPath = @"C:\Users\torch\work\DAFFODIL\DAFFODIL\src\lib\Microsoft.Torch.ExceptionFlowAnalysis\PtrAndExcAnalysis";
+            ConfigParams.LoadConfig(@"C:\Users\torch\work\DAFFODIL\DAFFODIL\src\test\T4\daffodil.cfg");
             ProgramDoms.Initialize();
             ProgramRels.Initialize();
-
-            ConfigParams.LoadSavedScope = false;
-            // ConfigParams.DatalogDir = @"C:\Users\torch\work\FromGithub\bmk1\storage\good\storage\src\Azure\Storage.Net.Microsoft.Azure.Storage\temp";
-            // ConfigParams.LogDir = @"C:\Users\torch\work\FromGithub\bmk1\storage\good\storage\src\Azure\Storage.Net.Microsoft.Azure.Storage\logs";
-            // ByteCodeAnalyzer.GenerateEDBFacts(@"C:\Users\torch\work\FromGithub\bmk1\storage\good\storage\src\Azure\Storage.Net.Microsoft.Azure.Storage\bin\Debug\net452\Storage.Net.Microsoft.Azure.Storage.dll");
-
-            ConfigParams.DatalogDir = @"C:\Users\torch\work\DAFFODIL\DAFFODIL\src\test\T4\temp";
-            ConfigParams.SaveScopePath = @"C:\Users\torch\work\DAFFODIL\DAFFODIL\src\test\T4\temp";
-            ConfigParams.LogDir = @"C:\Users\torch\work\DAFFODIL\DAFFODIL\src\test\T4\logs";
             ByteCodeAnalyzer.GenerateEDBFacts(@"C:\Users\torch\work\DAFFODIL\DAFFODIL\src\test\T4\bin\Debug\T4.exe");
-
-            // ConfigParams.DatalogDir = @"C:\Users\torch\work\Test\storage-blob-dotnet-getting-started\BlobStorage\temp";
-            // ConfigParams.SaveScopePath = @"C:\Users\torch\work\Test\storage-blob-dotnet-getting-started\BlobStorage\temp";
-            // ConfigParams.LogDir = @"C:\Users\torch\work\Test\storage-blob-dotnet-getting-started\BlobStorage\logs";
-            // ByteCodeAnalyzer.GenerateEDBFacts(@"C:\Users\torch\work\Test\storage-blob-dotnet-getting-started\BlobStorage\bin\Debug\BlobStorage.exe");
-
             ProgramDoms.Save();
             ProgramRels.Save();
-
             Z3CommandLineInvoke.CopyFiles(ConfigParams.DatalogDir);
-
             Z3CommandLineInvoke.LaunchZ3("CIPtrAnalysis.datalog", ConfigParams.DatalogDir);
             // Z3CommandLineInvoke.LaunchZ3("ExcAnalysisIntraProc.datalog", ConfigParams.DatalogDir);     
             // Z3CommandLineInvoke.LaunchZ3("ExcAnalysisInterProc.datalog", ConfigParams.DatalogDir);         
