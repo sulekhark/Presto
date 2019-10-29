2921ead987c4f3cb645c9df679349697d888811e
sulekhark <sulekha.kulkarni@gmail.com>
2019-10-29 03:34:53 -0400

    testcases

---
+++ DAFFODIL/src/test/T20/T20.cs
@@ -0,0 +1,41 @@
+﻿namespace T20
+{
+    class T20
+    {
+        static void Main(string[] args)
+        {
+            FooBase fooB = new FooBase();
+            Process(fooB);
+            FooDerived fooD = new FooDerived();
+            Process(fooD);
+        }
+
+        static void Process(IFooBase fooParam)
+        {
+            FooDerived fooParamD = fooParam as FooDerived;
+            if (fooParamD != null) fooParamD.FooDFunc();
+            fooParam.FooBFunc();
+        }
+    }
+
+    interface IFooBase
+    {
+        void FooBFunc();
+    }
+
+    interface IFooDerived : IFooBase
+    {
+        void FooDFunc();
+    }
+
+    class FooBase : IFooBase
+    {
+        public void FooBFunc() { }
+    }
+
+    class FooDerived : IFooDerived
+    {
+        public void FooBFunc() { }
+        public void FooDFunc() { }
+    }
+}
---
+++ DAFFODIL/src/test/TorchInst/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace TorchInst
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            Foo foo = new Foo();
+            string s = "Everything fine.";
+            try
+            {
+                A a = foo.FooEntry(b);
+                foo.FooProcess(a);
+            }
+            catch (Exception e)
+            {
+                s = e.Message;
+            }
+            finally
+            {
+                B result = foo.FooDoAsync();
+                Console.WriteLine("Done: {0}", s);
+            }
+            
+        }
+    }
+
+    class Foo
+    {
+        public A FooEntry(B bpri)
+        {
+            B bsec;
+            try
+            {
+                bsec = FooCallee();
+            }
+            catch (NullReferenceException nre)
+            {
+                throw nre;
+            }
+            A a = new A();
+            a.pri = bpri;
+            a.sec = bsec;
+            return a;
+        }
+
+        public B FooCallee()
+        {
+            B b = new B();
+            if (b == null)
+            {
+                throw new NullReferenceException();
+            }
+            return b;
+        }
+
+        public void FooProcess(A a)
+        {
+            if (a == null) throw new NullReferenceException();
+        }
+
+        public B FooDoAsync()
+        {
+            Task<B> res = DoAsyncWork();
+            res.Wait();
+            return res.Result;
+        }
+
+        public async Task<B> DoAsyncWork()
+        {
+            B res;
+            try
+            {
+                res = await MyAsyncTask();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return res;
+        }
+
+        public Task<B> MyAsyncTask()
+        {
+            Task<B> tmo;
+            B xxx;
+            tmo = Task.Run(() =>
+            {
+                xxx = new B();
+                if (xxx == null) throw new DivideByZeroException();
+                return xxx;
+            });
+            return tmo;
+        }
+    }
+
+    class A
+    {
+        public B pri;
+        public B sec;
+    }
+
+    class B { }
+}
