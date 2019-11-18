957c00d30c0783777f05f7e17f44d3f84cb8e852
Sulekha Kulkarni <sulekha.kulkarni@gmail.com>
2019-11-18 02:23:55 -0500

    cleaned up prep.sh

--- DAFFODIL/src/lib/Microsoft.Torch.ExceptionFlowAnalysis/Datalog/prep.sh
+++ DAFFODIL/src/lib/Microsoft.Torch.ExceptionFlowAnalysis/Datalog/prep.sh
@@ -3,31 +3,19 @@
 data_dir=$1
 
 cd $data_dir
-cp ~/work/ExcAnalysis/DAFFODIL/DAFFODIL/src/lib/Microsoft.Torch.ExceptionFlowAnalysis/Datalog/* .
-dos2unix CIPtrAnalysis.datalog
-echo -n "" >> CIPtrAnalysis.datalog
-dos2unix ExcAnalysisInterProc.datalog
-echo -n "" >> ExcAnalysisInterProc.datalog
-dos2unix ExcAnalysisIntraProc.datalog
-echo -n "" >> ExcAnalysisIntraProc.datalog
-dos2unix ExcFlows.datalog
-echo -n "" >> ExcFlows.datalog
-dos2unix DbgFlows.datalog
-echo -n "" >> DbgFlows.datalog
-dos2unix DbgTrack.datalog
-echo -n "" >> DbgTrack.datalog
-dos2unix parse_z3_out.py
-echo -n "" >> parse_z3_out.py
-dos2unix run_all.sh
-echo -n "" >> run_all.sh
-dos2unix run_dbg_flows.sh
-echo -n "" >> run_dbg_flows.sh
-dos2unix run_dbg_track.sh
-echo -n "" >> run_dbg_track.sh
+for i in ~/work/ExcAnalysis/DAFFODIL/DAFFODIL/src/lib/Microsoft.Torch.ExceptionFlowAnalysis/Datalog/*
+do
+    cp $i .
+    fname=`basename $i`
+    dos2unix $fname
+done
 cp ../logs/rta_log.txt .
 cp ../logs/tac_log.txt .
 dos2unix tac_log.txt
+dos2unix rta_log.txt
 dos2unix modules.txt
+dos2unix classes.txt
+dos2unix methods.txt
 
 cd ..
 tar czf $data_dir.tgz $data_dir 
