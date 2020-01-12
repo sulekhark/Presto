47323b45a7bf5069c16646c107c80b0d82f3aa53
Sulekha Kulkarni <sulekha.kulkarni@gmail.com>
2020-01-12 11:03:41 -0500

    Modified build.sh to suit fir02.seas.upenn.edu.

--- BnetTools/libdai/build.sh
+++ BnetTools/libdai/build.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
 curdir=`pwd`
-if [[ $HOSTNAME =~ "fir" || $HOSTNAME =~ "ae249" ]]; then
-   # fir machines do not have boost and libgmp installed. Therefore, we use pre-compiled libraries that we install in chord-fork/libs.
-   # Also, not all the libraries of boost are available in our installation (ex: -lboost_unit_test_framework is not available). 
-   # Therefore, we suppress the build of unittests, utils, etc and build only the libdai library, if the build happens on a fir machine. This check is done using HOSTNAME, in the Makefile 
-   make -j 8 CCINC="-I ../libs/boost_1_61_0 -I ../libs/gmp-6.1.0 -I include " CCLIB="-L ../libs/boost_1_61_0/installdir/lib -L ../libs/gmp-6.1.0/installdir/lib -L lib " HOSTNAME=$HOSTNAME
-   g++ -std=c++11 -O2 -march=native -Wall -Wextra -Werror -I ./include -I ../libs/boost_1_61_0 -I ../libs/gmp-6.1.0 -Wl,-rpath -Wl,$curdir/../libs/gmp-6.1.0/installdir/lib -fopenmp wrapper.cpp ./lib/libdai.a -L ../libs/gmp-6.1.0/installdir/lib -lgmp -lgmpxx -o wrapper
-   g++ -std=c++11 -O2 -march=native -Wall -Wextra -Werror -I ./include -I ../libs/boost_1_61_0 -I ../libs/gmp-6.1.0 -Wl,-rpath -Wl,$curdir/../libs/gmp-6.1.0/installdir/lib -fopenmp wrapper_jt.cpp ./lib/libdai.a -L ../libs/gmp-6.1.0/installdir/lib -lgmp -lgmpxx -o wrapper_jt
-else
-   make -j 8 && \
-   g++ -std=c++11 -O2 -march=native -Wall -Wextra -Werror -I ./include -lgmp -lgmpxx -fopenmp wrapper.cpp ./lib/libdai.a -o wrapper
-   g++ -std=c++11 -O2 -march=native -Wall -Wextra -Werror -I ./include -lgmp -lgmpxx -fopenmp wrapper_jt.cpp ./lib/libdai.a -o wrapper_jt
-fi
-
-exit $?
+make -j 8 CCINC="-I ../gmp-6.1.0 -I include " CCLIB="-L ../gmp-6.1.0/installdir/lib -L lib "
+g++ -std=c++11 -O2 -march=native -Wall -Wextra -Werror -I ./include -I ../gmp-6.1.0 -Wl,-rpath -Wl,$curdir/../gmp-6.1.0/installdir/lib -fopenmp wrapper.cpp ./lib/libdai.a -L ../gmp-6.1.0/installdir/lib -lgmp -lgmpxx -o wrapper
