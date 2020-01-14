9f1c5e046a2832618002e0552d440e2b8adbe6e0
Sulekha Kulkarni <sulekha.kulkarni@gmail.com>
2020-01-14 07:57:09 -0500

    Support to run LinkedEx configurations.

--- Scripts/run_torch.sh
+++ Scripts/run_torch.sh
@@ -9,6 +9,8 @@ mkdir dynlogs
 cd dynlogs
 mkdir Logging
 mkdir FaultInjectionSet
+mkdir FaultInjectionSet/EscapeMTP
+mkdir FaultInjectionSet/LinkedEx
 cd ..
 
 cp -r bin/Debug bin/Debug_orig
@@ -20,7 +22,7 @@ $TORCH_HOME/tools/net/TorchLogAnalyzer.exe decompress -i torch-runtime-log4net.t
 mv execution_log ../../dynlogs/Logging
 mv torch-runtime-log4net.torchlog.decompressed ../../dynlogs/Logging
 
-while read -r dirName 
+while read -r dirName
 do
     cd ..
     rm -rf Debug
@@ -28,13 +30,29 @@ do
     cp Debug_orig/* Debug
     cd Debug
 
-    $TORCH_HOME/tools/net/torch-instrumenter-internal.exe -i . --ic ../../dynconfig/FaultInjectionSet/$dirName/torch-instrumentation.torchconfig  --rc ../../dynconfig/FaultInjectionSet/$dirName/RuntimeConfig
+    $TORCH_HOME/tools/net/torch-instrumenter-internal.exe -i . --ic ../../dynconfig/FaultInjectionSet/EscapeMTP/$dirName/torch-instrumentation.torchconfig  --rc ../../dynconfig/FaultInjectionSet/EscapeMTP/$dirName/RuntimeConfig
     ./E2EDemo.exe > execution_log 2>&1
     $TORCH_HOME/tools/net/TorchLogAnalyzer.exe decompress -i torch-runtime-log4net.torchlog
-    mkdir ../../dynlogs/FaultInjectionSet/$dirName
-    mv execution_log ../../dynlogs/FaultInjectionSet/$dirName
-    mv torch-runtime-log4net.torchlog.decompressed ../../dynlogs/FaultInjectionSet/$dirName
-done < ../../dynconfig/FaultInjectionSet/fault_config_dirs.txt
+    mkdir ../../dynlogs/FaultInjectionSet/EscapeMTP/$dirName
+    mv execution_log ../../dynlogs/FaultInjectionSet/EscapeMTP/$dirName
+    mv torch-runtime-log4net.torchlog.decompressed ../../dynlogs/FaultInjectionSet/EscapeMTP/$dirName
+done < ../../dynconfig/FaultInjectionSet/EscapeMTP/fault_config_dirs.txt
+
+while read -r dirName
+do
+    cd ..
+    rm -rf Debug
+    mkdir Debug
+    cp Debug_orig/* Debug
+    cd Debug
+
+    $TORCH_HOME/tools/net/torch-instrumenter-internal.exe -i . --ic ../../dynconfig/FaultInjectionSet/LinkedEx/$dirName/torch-instrumentation.torchconfig  --rc ../../dynconfig/FaultInjectionSet/LinkedEx/$dirName/RuntimeConfig
+    ./E2EDemo.exe > execution_log 2>&1
+    $TORCH_HOME/tools/net/TorchLogAnalyzer.exe decompress -i torch-runtime-log4net.torchlog
+    mkdir ../../dynlogs/FaultInjectionSet/LinkedEx/$dirName
+    mv execution_log ../../dynlogs/FaultInjectionSet/LinkedEx/$dirName
+    mv torch-runtime-log4net.torchlog.decompressed ../../dynlogs/FaultInjectionSet/LinkedEx/$dirName
+done < ../../dynconfig/FaultInjectionSet/LinkedEx/fault_config_dirs.txt
 
 # Restore original state - i.e. before instrumentation
 cd ..
