41a558f1d2322fe677788fcfc90700e3ca29e40d
Sulekha Kulkarni <sulekha.kulkarni@gmail.com>
2020-05-13 23:55:26 -0400

    Organization.

--- DAFFODIL/src/stubs/Daffodil.Stubs/IAsyncStateMachine.cs
+++ DAFFODIL/src/stubs/Daffodil.Stubs/IAsyncStateMachine.cs
@@ -1,3 +1,5 @@
+// Copyright (c) Sulekha Kulkarni.  All Rights Reserved.  Licensed under the MIT License.  See License.txt in the project root for license information.
+
 ﻿
 
 namespace Daffodil.Stubs
