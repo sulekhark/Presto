82d0687fa1bd5b1a02abf21cb37510c27f183e99
Sulekha Kulkarni <sulekha.kulkarni@gmail.com>
2020-07-07 15:06:34 -0400

    Fix to control what gets instrumented during dynamic analysis.

--- DynamicConfig/scripts/get_assemblies.sh
+++ DynamicConfig/scripts/get_assemblies.sh
@@ -2,11 +2,23 @@
 
 modulesFile=$1
 outputFile=$2
+blacklistFile="$PRESTO_HOME/DynamicConfig/scripts/blacklist.txt"
 
 rm -f $outputFile
 while read -r line
 do
     fname=`echo $line | awk -F ':' '{print $4;}'`
     asmName=`echo $fname | awk -F '\' '{print $NF;}'`
-    echo $asmName >> $outputFile
+    exclude=0
+    while read -r pre
+    do
+        if [[ $asmName == $pre* ]]
+        then
+            exclude=1
+        fi
+    done < $blacklistFile
+    if [ $exclude -eq 0 ]
+    then
+        echo $asmName >> $outputFile
+    fi
 done < $modulesFile
